475fc7dbf495b226c72918024e977ea3354a9e54
ardhi024 <gedeardhiyana@gmail.com>
2024-01-01 12:24:39 +0800

    update data
    Signed-off-by: ardhi024 <gedeardhiyana@gmail.com>

--- index.js
+++ index.js
@@ -5,11 +5,5 @@ app.get('/', function (req, res) {
     res.send('hello world');
 });
 
-app.get('/will', function (req, res) {
-    res.send('{ "response": "implementasi cicd" }');
-});
-app.get('/ready', function (req, res) {
-    res.send('{ "response": "Berhasil Sukses!" }');
-});
 app.listen(process.env.PORT || 3000);
 module.exports = app;
